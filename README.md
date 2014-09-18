eea455429743aa47bea5c4d2721e562a0d97bdd9
Eugene Kadish <eugenekadish@gmail.com>
2014-09-18 11:13:17 -0400

    General cleanup of the entire project.

--- README.md
+++ README.md
@@ -1,26 +1,105 @@
 # Trackball
-> Accurately rotate a 3D scene. 
 
-## What's this for?
+> Enables accurate 3D rotations.
 
-Naively stacking rotation matrices result in some unintended effects as numeric errors pile up when very large numbers of rotation matrices are multiplied. The *Trackball* overcomes this with the use of a quaternion to represent the rotation. Using a quaternion has the added advantage that it can be normalized, which effectively resets the numerical error associated with the rotation.
+## Summary
 
-## Set Up and Usage
+When applying transformations in 3D space, stacking rotation matrices can lead to unintended consequences as a result of numerical error. The rotations become imprecise occasionally falling a full 180 degrees out of phase, and may cause additional anomalies such as gimbal lock. A solution to these and other unintended consequences is the use of a mathematical construct called [quaternions](http://en.wikipedia.org/wiki/Quaternion). These are four element tuples that extend the complex numbers. Each such four dimensional vector can represent a rotation in three dimensions, and can be converted to the associated rotation matrix. The quaternion offers several advantages for handling compositions of rotations over straight forward matrix multiplication, including ease of computation, compact representation, and solves the inaccuracies mentioned. Two rotations represented by quaternions can be composed into one by a vector product, a calculation that requires a fraction of the operations needed to perform the corresponding matrix multiplication. Also, since changing the magnitude of a quaternion will not affect the rotation it represents, after each composition of two rotations the resultant vector can be normalized before cast as a rotation matrix. This additional step removes the issues experienced with a straightforward matrix products, as numerical errors are smoothed out during normalization and do not accumulate. This module abstracts away the mathematics and applies quaternions to enable accurate user interaction of a rotation transform in a 3D scene. Additionally, vertex and fragment shaders are provided in the *example* that perform texturing.
 
-In order to use *trackball.js* and run all the tests and examples it comes with, download [jquery](http://jquery.com/), [qunit](http://qunitjs.com/), and the [gl-matrix](https://github.com/toji/gl-matrix) matrix and vector javascript library. To avoid having to get these manually run
+## Setup
+
+The dependencies are the [gl-matrix](https://github.com/toji/gl-matrix) matrix and javascript library, along with the [qunit](http://qunitjs.com) unit testing framework. To run the tests and verify everything is working correctly, navigate to the *runner.html* file in the tests directory with any modern browser. To avoid downloading these packages manually, the command
 
     bower install
 
-to download the libraries into a folder called *components* at the root directory. Next, simply include the javascript file that contains this module in your page.
+will fetch the required libraries and place them into the *components* folder at the root directory. The [bower](http://bower.io) command requires [node.js](http://nodejs.org) to be installed.
+
+    npm install bower
+
+will download the package.
+
+## Viewing
+
+To checkout how this module can be used, open the contents of the *example* folder after all the dependencies have been successfully downloaded or just peep [demo](http://eugenekadish.github.io/trackball).
+
+## Documentation
+
+    __Trackball(__*mode*__,__*scene*__)__
+
+    Wrapper for emulating rotation transformations. This method must be called in order to instantiate and make available all other functions. The transformation is internally stored based on the boolean *mode* parameter. Setting it to __true__ uses the intended quaternion representation. With __false__ the transformations will be done solely with matrix multiplication. This functionality could be used to quickly compare the advantages of the quaternion approach. The second parameter, *scene*, is the *canvas* tag the transformations will be used in. It provides dimensional parameters that are needed to accurately interpolate the cursor position.
+    
+    __this.orientation(__*v*__)__
+    
+    Calculates the angular proximity of six rectilinear unit vectors from *v*. Where *v* is a three dimensional vector represented simply as a three element array. The value returned is the index of the unit vector closest to *v* as enumerated in the list.
+
+    0 : <  1,  0,  0 >
+    1 : < -1,  0,  0 >
+    2 : <  0,  1,  0 >
+    3 : <  0, -1,  0 >
+    4 : <  0,  0,  1 >
+    5 : <  0,  0, -1 >
+
+    This function can be useful in determining the relative state of the current transformation.
+
+    __this.interpolate(__*xPos*__,__*yPos*__)__
+    
+    Maps the horizontal and vertical raw pixel coordinates of the cursor position to the range [-1, 1]. *xPos* and *yPos* are the horizontal and vertical pixel position of the cursor in the window respectively. The return value is a two element tuple, which is the horizontal and vertical pixel positions of the cursor mapped to the range [-1, 1].
+
+    __this.push(__*xPos*__,__ *yPos*__)__
+    
+    Initializes internal state of the rotation transformation. *xPos* and *yPos* are the interpolated horizontal and vertical positions of the cursor each mapped to be in the range [-1, 1]. 
+
+    this.move(*xPos*, *yPos*)
+    
+    Updates the internal state of the rotation transformation. *xPos* and *yPos* are the interpolated horizontal and vertical positions of the cursor, each have to be interpolated to be in the range [-1, 1]. 
+
+    this.release()
+    
+    Disables updating of the internal state of the transformation.
+
+## Usage
+
+Include the source file
 
 ```html
-   <script src="path/to/trackball.js"> </script>
+   <script src="path/to/trackball.js"></script>
 ```
 
-*Trackball* provides methods to call when first pressing, moving, and releasing the mouse on the widget. For emulating the rotation the internal representation can be switched between the quaternion and a naive rotation matrix representation by setting the *mode* when initially creating the object. For example the following code will use a quaternion for the trackball.
+in the page. Typically the functions provided would be used in conjunction with native javascript or [jQuery](http://jquery.com) methods that enable user interaction. Here is a small snippet from the example using the package.
 
 ```javascript
-   var t = new Trackball(true);
+  
+  var scene = document.getElementById('scene');
+    . . .
+
+  var t = new Trackball(true);
 ```
 
-Pass in *false* for the other mode and compare the great benefits that come with quaternions.
+```javascript
+  
+  var cursor;
+
+  scene.onmousedown = function(event){
+
+    cursor = t.interpolate(event.clientX, event.clientY);
+
+    // t.stop();
+    t.push(cursor[0], cursor[1]);
+  };
+
+  scene.onmousemove = function(event){
+
+    cursor = t.interpolate(event.clientX, event.clientY);
+
+    t.move(cursor[0], cursor[1]);
+      
+      . . .
+  };
+
+  document.onmouseup = function(event){
+      
+    t.release();
+    // t.start();
+  };
+
+```
--- example/scripts/scripts.js
+++ example/scripts/scripts.js
@@ -1,28 +1,30 @@
 /**
-  * A simple static scene is displayed when the page is loaded with click and drag UI functionality.
-  */
+ * Widget that displays a dynamic graphic with click and drag mouse functionality.
+ */
 (function(){
+  
+  var scene = document.getElementById('scene');
 
-   // Check the if the web browser can display 3D graphics.
-   try {
-      var gl = document.getElementById('scene').getContext('webgl');
-   } catch(e){
-      if(!gl){
-         alert(' WebGL could not be initialized.');
-      }
-   }
+  // Check that the browser has WebGL support.
+  try {
+    var gl = scene.getContext('webgl');
+  } catch(e){
+    if(!gl){
+      alert(' WebGL could not be initialized.');
+    }
+  }
 
-   var windowWidth  = document.getElementById('scene').clientWidth;
-   var windowHeight = document.getElementById('scene').clientHeight;
+  var windowWidth  = scene.width;
+  var windowHeight = scene.height;
 
-   gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.DEPTH_TEST);
 
-   // Buffer containing coordinate data of a unit cube centered about the origin.
-   var vertexBuffer = gl.createBuffer();
+  // Buffer containing coordinate data of a unit cube centered about the origin.
+  var vertexBuffer = gl.createBuffer();
     
-   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-   gl.bufferData(gl.ARRAY_BUFFER,
-                     new Float32Array([
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER,
+                    new Float32Array([
                           -0.5, -0.5,  0.5,  1.0,  0.5, -0.5,  0.5,  1.0,  0.5,  0.5,  0.5,  1.0,
                            0.5,  0.5,  0.5,  1.0, -0.5,  0.5,  0.5,  1.0, -0.5, -0.5,  0.5,  1.0,
                           -0.5,  0.5, -0.5,  1.0,  0.5,  0.5, -0.5,  1.0,  0.5, -0.5, -0.5,  1.0, 
@@ -34,15 +36,15 @@
                            0.5,  0.5,  0.5,  1.0,  0.5,  0.5, -0.5,  1.0, -0.5,  0.5, -0.5,  1.0,
                           -0.5,  0.5,  0.5,  1.0,  0.5,  0.5,  0.5,  1.0, -0.5,  0.5, -0.5,  1.0,
                            0.5, -0.5,  0.5,  1.0, -0.5, -0.5,  0.5,  1.0, -0.5, -0.5, -0.5,  1.0,
-                          -0.5, -0.5, -0.5,  1.0,  0.5, -0.5, -0.5,  1.0,  0.5, -0.5,  0.5,  1.0 
-                                       ]),
-                              gl.STATIC_DRAW);
+                          -0.5, -0.5, -0.5,  1.0,  0.5, -0.5, -0.5,  1.0,  0.5, -0.5,  0.5,  1.0
+                                      ]),
+                                gl.STATIC_DRAW);
    
-   var textureBuffer = gl.createBuffer();
+  var textureBuffer = gl.createBuffer();
           
-   gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-   gl.bufferData(gl.ARRAY_BUFFER, 
-                      new Float32Array([
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 
+                    new Float32Array([
                             0.00, 0.00, 0.35, 0.00, 0.35, 0.50,
                             0.35, 0.50, 0.00, 0.50, 0.00, 0.00,
                             0.36, 0.50, 0.67, 0.50, 0.67, 0.00,
@@ -56,162 +58,108 @@
                             1.00, 0.50, 0.66, 0.50, 0.66, 0.00,
                             0.66, 0.00, 1.00, 0.00, 1.00, 0.50
                                       ]), 
-                            gl.STATIC_DRAW);
-
-   // Define and create the shaders.
-   var sources = [];
-   var shaders = [];
-
-   sources[0] = 'attribute vec4 vertex;' +
-                'attribute vec2 texture;' +
-                
-                'uniform mat4 projection;' +
-                'uniform mat4 modelview;' + 
-                
-                'varying vec2 t;' +
-
-                'void main(){' +
-                '  t = texture;' +
-                '  gl_Position = projection * modelview * vertex;' +
-                '}';
-
-   sources[1] = 'precision mediump float;' +
-           
-                'varying vec2 t;' +
-
-                'uniform sampler2D sampler;' +
-                   
-                'void main(){' +
-                '  gl_FragColor = texture2D(sampler, vec2(t.s, t.t));' +
-                '}';
-
-   shaders[0] = gl.createShader(gl.VERTEX_SHADER);
-   shaders[1] = gl.createShader(gl.FRAGMENT_SHADER);
-
-   gl.shaderSource(shaders[0], sources[0]);
-   gl.compileShader(shaders[0]);
-
-   gl.shaderSource(shaders[1], sources[1]);
-   gl.compileShader(shaders[1]);
-
-   // Check to see that the shaders compiled.
-   for(var i = 0; i < shaders.length; i++){
-      var shader = shaders[i];
-      if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-         alert(gl.getShaderInfoLog(shader));
-      }
-   }
-
-   var shaderProgram = gl.createProgram();
-      
-   gl.attachShader(shaderProgram, shaders[0]);
-   gl.attachShader(shaderProgram, shaders[1]);
-   gl.linkProgram(shaderProgram);
-
-   // Check to see if the program linked.
-   if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-      alert(' The shaders could not be initialized.');
-   }
-
-   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-   gl.viewport(0, 0, windowWidth, windowHeight);
-      
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-   gl.useProgram(shaderProgram);
-
-   // Set up the matrices for the viewing transformation.
-   var eye    = vec3.create();
-   var center = vec3.create();
-   var up     = vec3.create();
-
-   vec3.set(eye, 0.0, 0.0, 3.0);
-   vec3.set(center, 0.0, 0.0, 0.0);
-   vec3.set(up, 0.0, 1.0, 0.0);
+                                gl.STATIC_DRAW);
   
-   var projectionMatrix         = mat4.create();
-   var modelviewMatrix          = mat4.create();
-   var temporaryModelviewMatrix = mat4.create();
-   var rotationMatrix           = mat4.create();
+  var shaderProgram = shaderImport(['shaders/vertex.glsl', 'shaders/fragment.glsl'], gl);
+  
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.viewport(0, 0, windowWidth, windowHeight);
+  gl.useProgram(shaderProgram);
 
-   mat4.frustum(projectionMatrix, -0.5, 0.5, -0.5, 0.5, 1.0, 15.0);
-   mat4.lookAt(modelviewMatrix, eye, center, up);
+  // Set up the matrices for the viewing transformation.
+  var eye    = vec3.create();
+  var center = vec3.create();
+  var up     = vec3.create();
 
-   shaderProgram.vertexAttribute  = gl.getAttribLocation(shaderProgram, 'vertex'); 
-   shaderProgram.textureAttribute = gl.getAttribLocation(shaderProgram, 'texture');
+  vec3.set(eye, 0.0, 0.0, 3.0);
+  vec3.set(center, 0.0, 0.0, 0.0);
+  vec3.set(up, 0.0, 1.0, 0.0);
+  
+  var projectionMatrix         = mat4.create();
+  var modelviewMatrix          = mat4.create();
+  var temporaryModelviewMatrix = mat4.create();
+  var rotationMatrix           = mat4.create();
 
-   // Set the attribute locations and point them to the corresponding buffer data.
-   gl.enableVertexAttribArray(shaderProgram.vertexAttribute);
-   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-   gl.vertexAttribPointer(shaderProgram.vertexAttribute, 4, gl.FLOAT, false, 0, 0);
+  mat4.frustum(projectionMatrix, -0.5, 0.5, -0.5, 0.5, 1.0, 15.0);
+  mat4.lookAt(modelviewMatrix, eye, center, up);
 
-   gl.enableVertexAttribArray(shaderProgram.textureAttribute);
-   gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-   gl.vertexAttribPointer(shaderProgram.textureAttribute, 2, gl.FLOAT, false, 0, 0);
+  shaderProgram.vertexAttribute  = gl.getAttribLocation(shaderProgram, 'vertex'); 
+  shaderProgram.textureAttribute = gl.getAttribLocation(shaderProgram, 'texture');
 
-   // Set the locations and data of uniform variables.
-   shaderProgram.projectionUniform = gl.getUniformLocation(shaderProgram, 'projection');
-   shaderProgram.modelviewUniform  = gl.getUniformLocation(shaderProgram, 'modelview');
-   shaderProgram.samplerUniform    = gl.getUniformLocation(shaderProgram, "sampler");
+  // Set the attribute locations and point them to the corresponding buffer data.
+  gl.enableVertexAttribArray(shaderProgram.vertexAttribute);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexAttribute, 4, gl.FLOAT, false, 0, 0);
 
-   gl.uniformMatrix4fv(shaderProgram.projectionUniform, false, projectionMatrix);
-   gl.uniformMatrix4fv(shaderProgram.modelviewUniform, false, modelviewMatrix);
+  gl.enableVertexAttribArray(shaderProgram.textureAttribute);
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+  gl.vertexAttribPointer(shaderProgram.textureAttribute, 2, gl.FLOAT, false, 0, 0);
 
-    // Set up the texture.
-   var texture = gl.createTexture();
+  // Set the locations and data of uniform variables.
+  shaderProgram.projectionUniform = gl.getUniformLocation(shaderProgram, 'projection');
+  shaderProgram.modelviewUniform  = gl.getUniformLocation(shaderProgram, 'modelview');
+  shaderProgram.samplerUniform    = gl.getUniformLocation(shaderProgram, "sampler");
+
+  gl.uniformMatrix4fv(shaderProgram.projectionUniform, false, projectionMatrix);
+  gl.uniformMatrix4fv(shaderProgram.modelviewUniform, false, modelviewMatrix);
+ 
+  gl.uniform1i(shaderProgram.samplerUniform, 0);
+  
+  // Set up a texture.
+  var texture = gl.createTexture(); 
+  
+  gl.activeTexture(gl.TEXTURE0);
+ 
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
           
-   texture.image     = new Image();
-   texture.image.onload = function(){
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-      gl.bindTexture(gl.TEXTURE_2D, null);
+  texture.image = new Image(); 
+  texture.image.src = "images/icons.jpg";
+  
+  texture.image.onload = function(){
+    
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+    
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);    
+    gl.drawArrays(gl.TRIANGLES, 0, 36);
+  }
 
-      gl.activeTexture(gl.TEXTURE0);
-      gl.bindTexture(gl.TEXTURE_2D, texture);
+  // Enable click and drag functionality for the triangles in the buffer.
+  // var t = new Trackball(true);
+  var t = new Trackball(true, scene);  
+
+  var cursor;
+
+  scene.onmousedown = function(event){
+
+    cursor = t.interpolate(event.clientX, event.clientY);
+
+    // t.stop();
+    t.push(cursor[0], cursor[1]);
+  };
+
+  scene.onmousemove = function(event){
+
+    cursor = t.interpolate(event.clientX, event.clientY);
+
+    t.move(cursor[0], cursor[1]);
+ 
+    // Get the updated rotation matrix.
+    mat4.multiply(temporaryModelviewMatrix, modelviewMatrix, t.rotation());
+
+    // Update the modelview matrix.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.uniformMatrix4fv(shaderProgram.modelviewUniform, false, temporaryModelviewMatrix);
       
-      gl.uniform1i(shaderProgram.samplerUniform, 0);
-   }
+    gl.drawArrays(gl.TRIANGLES, 0, 36);
+  };
 
-   texture.image.src = "images/icons.jpg";
-   
-   gl.drawArrays(gl.TRIANGLES, 0, 36);
-
-   // Enable click and drag functionality for the triangles in the buffer.
-   var t = new Trackball(true);
-
-   var cursor;
-
-   $('#scene').mousedown(function(event){
-
-      cursor = t.interpolate(event.clientX, event.clientY);
-
-      // t.stop();
-      t.push(cursor[0], cursor[1]);
-   });
-
-   $('#scene').mousemove(function(event){
-
-      cursor = t.interpolate(event.clientX, event.clientY);
-
-      t.move(cursor[0], cursor[1]);
-
-      // Get the updated rotation matrix.
-      mat4.multiply(temporaryModelviewMatrix, modelviewMatrix, t.rotation());
-
-      // Update the modelview matrix.
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      gl.uniformMatrix4fv(shaderProgram.modelviewUniform, false, temporaryModelviewMatrix);
+  document.onmouseup = function(event){
       
-      gl.drawArrays(gl.TRIANGLES, 0, 36);
-   });
+    t.release();
+    // t.start();
+  };
 
-   $('body').mouseup(function(event){
-      
-      t.release();
-      // t.start();
-   });
 })();
